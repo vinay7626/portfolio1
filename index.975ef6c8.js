e957bc907dec0cc54ed4537a48d4a2ec5ff726cb
vinay7626 <97014123+vinay7626@users.noreply.github.com>
2024-06-15 00:31:00 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -28759,24 +28759,24 @@ const imageAltText = "desktop with books and laptop";
  * links section. Below is a sample, update to reflect links you'd like to highlight.
  */ const projectList = [
     {
-        title: "10 Things To Know About Azure Static Web Apps \uD83C\uDF89",
-        description: "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-        url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i"
+        title: "Dribbble",
+        description: "This is a Full stack MERN web app I designed where the user can register by filling up a registration form.",
+        url: "https://dribbblevinay.netlify.app/"
     },
     {
-        title: "Web Development for Beginners",
-        description: "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-        url: "https://github.com/microsoft/web-dev-for-beginners"
+        title: "Employee Management System",
+        description: "A MERN stack CRUD application designed to streamline the storage and retrieval of the Employee Information with in the organization.",
+        url: "https://github.com/vinay7626/MERN-stack-CRUD-application"
     },
     {
-        title: "My Resume Site",
-        description: "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-        url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website"
+        title: "Learning Bootstrap",
+        description: "This is a landing page I designed using bootstrap Grid layout. It is fully responsive for all devices and screen sizes.",
+        url: "https://github.com/vinay7626/HTML_Bootstrap_project"
     },
     {
-        title: "GitHub Codespaces and github.dev",
-        description: "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-        url: "https://www.youtube.com/watch?v=c3hHhRME_XI"
+        title: "Realtime Chat App (Ongoing project)",
+        description: "This is a Chat application that i will be hosting where the user can create an account and then chat with anyone else in real time using socket.io",
+        url: "#"
     }, 
 ];
 const Portfolio = ()=>{
